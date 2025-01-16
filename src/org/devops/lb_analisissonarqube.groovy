8d8176e4e9316a20d4413ed803da97a744f848d4
Juan J. Cita Sarmiento <juancita@periferia-it.com>
2025-01-16 09:59:28 -0500

    Creación de la rama feature para el reto

--- src/org/devops/lb_analisissonarqube.groovy
+++ src/org/devops/lb_analisissonarqube.groovy
@@ -1,27 +1,28 @@
 package org.devops
 
 def testCoverage() {
-    // Ejecuta las pruebas con npm
-    sh 'npm test -- --coverage' // Ejecuta las pruebas generando cobertura
+    // Ejecuta las pruebas con npm y genera cobertura
+    sh 'npm test -- --coverage'
 }
 
 def analisisSonar(gitName) {
     // Define la variable scannerHome para la ruta del sonar-scanner
     def scannerHome = tool 'sonar-scanner'
-    
+
     // Verifica si sonar-scanner está disponible
     if (scannerHome) { 
         // Ejecuta el análisis con el sonar-scanner
         withSonarQubeEnv("sonar-scanner") {
-            sh "${scannerHome}/bin/sonar-scanner \
-            -Dsonar.projectKey=${gitName} \
-            -Dsonar.projectName=${gitName} \
-            -Dsonar.sources=src \
-            -Dsonar.tests=src/_test_ \
-            -Dsonar.exclusions=**/*.test.js \
-            -Dsonar.testExecutionReportPaths=./test-report.xml \
-            -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info
-            "
+            sh """
+                ${scannerHome}/bin/sonar-scanner \
+                -Dsonar.projectKey=${gitName} \
+                -Dsonar.projectName=${gitName} \
+                -Dsonar.sources=src \
+                -Dsonar.tests=src/_test_ \
+                -Dsonar.exclusions=**/*.test.js \
+                -Dsonar.testExecutionReportPaths=./test-report.xml \
+                -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info
+            """
         }
     } else {
         error 'SonarQube Scanner not found'
