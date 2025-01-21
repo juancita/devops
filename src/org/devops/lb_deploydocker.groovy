840f3802468a7cdd0035b1761c01d21628fdc9d7
Juan J. Cita Sarmiento <juancita@periferia-it.com>
2025-01-21 12:04:03 -0500

    Subiendo toda la configuracion para el reto fase 2

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -1,7 +1,7 @@
 package org.devops
 
 def despliegueContenedor(projectGitName){
-    sh "docker pull juancita/${projectGitName.toLowerCase()}"
+    sh "docker pull lainaus/${projectGitName.toLowerCase()}"
     sh """docker run -d --name ${projectGitName.toLowerCase()} \
     --network=${env.NameNetwork} -p 5174:5174 \
     --user root juancita/${projectGitName.toLowerCase()}
