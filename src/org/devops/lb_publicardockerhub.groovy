d889aa0fe6ca787589d29c6c69021ba757c08f4f
Juan J. Cita Sarmiento <juancita@periferia-it.com>
2025-01-21 11:48:02 -0500

    Subiendo toda la configuracion para el reto fase 2

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -1,9 +1,9 @@
 package org.devops
 
-def publicarImage(projectGitName){
+def publicarImage(projectGitName) {
     withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}", passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
         sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
-        sh "docker tag ${projectGitName} ${env.DOCKERHUB_USERNAME}/${projectGitName}"
-        sh "docker push  ${env.DOCKERHUB_USERNAME}/${projectGitName}"
+        sh "docker tag juancita/${projectGitName} ${env.DOCKERHUB_USERNAME}/${projectGitName}"
+        sh "docker push ${env.DOCKERHUB_USERNAME}/${projectGitName}"
     }
 }
