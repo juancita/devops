4e9da5726b0482f6965775c1f31f23c5c3d67f79
Juan J. Cita Sarmiento <juancita@periferia-it.com>
2025-01-21 11:56:55 -0500

    Subiendo toda la configuracion para el reto fase 2

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -1,11 +1,16 @@
 package org.devops
 
 def despliegueContenedor(projectGitName){
-    //sh "docker stop ${projectGitName}"
-    //sh "docker rm ${projectGitName}"
-    sh "docker pull juancita/RetoJenkinsFuncional"
-    sh """ docker run -d --name ${projectGitName} \
+    // Detener y eliminar cualquier contenedor previo si es necesario
+    // sh "docker stop ${projectGitName.toLowerCase()}"
+    // sh "docker rm ${projectGitName.toLowerCase()}"
+
+    // Usar un nombre fijo para la imagen en el pull
+    sh "docker pull juancita/retojenkinsfuncional"
+
+    // Correr el contenedor usando el projectGitName como nombre, asegurando que sea minúscula
+    sh """docker run -d --name ${projectGitName.toLowerCase()} \
     --network=${env.NameNetwork} -p 5174:5174 \
-    --user root juancita/${projectGitName}
+    --user root juancita/retojenkinsfuncional
     """
 }
