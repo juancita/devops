759cb0d3330d0552c97f8f17d4e2fe012f8fb9ce
Juan J. Cita Sarmiento <juancita@periferia-it.com>
2025-01-21 12:04:40 -0500

    Actualzianzdo juancita por LAinaus

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -4,6 +4,6 @@ def despliegueContenedor(projectGitName){
     sh "docker pull lainaus/${projectGitName.toLowerCase()}"
     sh """docker run -d --name ${projectGitName.toLowerCase()} \
     --network=${env.NameNetwork} -p 5174:5174 \
-    --user root juancita/${projectGitName.toLowerCase()}
+    --user root lainaus/${projectGitName.toLowerCase()}
     """
 }
