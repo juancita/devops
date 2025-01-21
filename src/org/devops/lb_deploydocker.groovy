65dc2bda563dec82259925daedbe8a6c8635f1e0
Juan J. Cita Sarmiento <juancita@periferia-it.com>
2025-01-21 12:07:23 -0500

    Actualzianzdo juancita por LAinaus y añadiendo el borrar y el parar

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -1,6 +1,8 @@
 package org.devops
 
 def despliegueContenedor(projectGitName){
+    sh "docker stop ${projectGitName.toLowerCase()}"
+    sh "docker rm ${projectGitName.toLowerCase()}"
     sh "docker pull lainaus/${projectGitName.toLowerCase()}"
     sh """docker run -d --name ${projectGitName.toLowerCase()} \
     --network=${env.NameNetwork} -p 5174:5174 \
